134893e2ed3ee0b8d5926ca9c88f8ca277438f37
FullZero <fullzero5@gmail.com>
2016-10-07 14:08:41 +0000

    include chenge

--- venyoo.php
+++ venyoo.php
@@ -36,14 +36,17 @@ class VenyooPlugin extends Plugin
         $id = trim($this->grav['config']->get('plugins.venyoo.widget_id'));
         if ($id) {
             $init = "
-              (function() {
-                var po = document.createElement('script');
-                    po.type = 'text/javascript';
-                    po.async = true;
-                    po.src = '//api.venyoo.ru/wnew.js?wc=venyoo/default/science&widget_id='+{$id};
-                var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(po, s);
-              })();
+              (function(d, w, c) {
+                var n = d.getElementsByTagName('script')[0],
+                    s = d.createElement('script');
+                w[c] = w[c] || function() {
+                    (w[c].q = w[c].q || []).push(arguments);
+                };
+                s.async = true;
+                s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+                    + '//api.venyoo.ru/wnew.js?wc=venyoo/default/science&widget_id='+{$id};
+                n.parentNode.insertBefore(s, n);
+            })(document, window, 'Venyoo');
         ";
         $this->grav['assets']->addInlineJs($init);
     }
